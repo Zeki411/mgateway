14444645ebee335ec3115a8cb7926dbc40e81515
Zeki411 <42540848+Zeki411@users.noreply.github.com>
2020-03-24 15:11:13 +0700

    Update PacketForwarder

--- apps/TCPPacketForwarder.py
+++ apps/TCPPacketForwarder.py
@@ -1,26 +1,59 @@
 import os
 import time
 import socket
+from threading import Thread
+import threading
+import sys
 
 class PCconnection:
         def __init__(self): 
-                self.host = '202.191.56.103' 
-                self.port = 5540 
-                self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
-                self.s.connect((self.host,self.port)) 
-                print("Connected") 
+            self.host = '192.168.16.102' 
+            self.port = 23 
+            self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+            self.s.connect((self.host,self.port)) 
+            print("Connected") 
+
         def send_Data(self, msg): 
-                self.s.send(msg.encode())
-                time.sleep(1)
+            self.s.send(msg.encode())
+            time.sleep(1)
+
+        def recv_Data(self, rxdata):
+            rxdata = (self.s.recv(4096)).decode('utf8')
+            if not rxdata: sys.exit(0)
+            print(rxdata)
+            with open(rxDir, 'a+') as fout:
+                fout.writelines(str(rxdata))
 
 myCon = PCconnection()
 
-while 1: 
-    if(os.stat('/opt/mgateway/apps/parsedData.csv').st_size!=0):
-        with open('/opt/mgateway/apps/parsedData.csv', 'r') as fin:
-            data = fin.read().splitlines(True)
-            myCon.send_Data(data[0])
-            print('Send Data to Server\n')
+def SendThread(txDir):
+    while True:
+        if(os.stat(txDir).st_size!=0):
+            with open(txDir, 'r') as fin:
+                data = fin.read().splitlines(True)
+                myCon.send_Data(data[0])
+                print('Send Data to Server\n')
 
-        with open('/opt/mgateway/apps/parsedData.csv', 'w') as fout:
-            fout.writelines(data[1:])
+            with open(txDir, 'w') as fout:
+                fout.writelines(data[1:])
+
+def RecvThread(rxDir):
+    while True:
+        rxdata = 0
+        myCon.recv_Data(rxdata)
+        
+
+
+txDir = './txData.csv'
+rxDir = './rxData.csv'
+
+try:
+	t = time.time()
+	t1 = threading.Thread(target=SendThread, args=(txDir,))
+	t2 = threading.Thread(target=RecvThread, args=(rxDir,))
+	t1.start()
+	t2.start()
+	t1.join()
+	t2.join()
+	print ("done in ", time.time()- t)
+except Exception as e: print(e)
